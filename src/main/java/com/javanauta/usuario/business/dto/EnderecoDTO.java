b7fa39cb0dd43ad7f1359956271137d4f4c01d0e
Alissongdb <alisson.gabriel5@hotmail.com>
2025-04-15 11:31:47 -0300

    adicionado metodo que salva dados de usuario

---
+++ src/main/java/com/javanauta/usuario/business/dto/EnderecoDTO.java
@@ -0,0 +1,18 @@
+package com.javanauta.usuario.business.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoDTO {
+
+    private String rua;
+    private Long numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+}
